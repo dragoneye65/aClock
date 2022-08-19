092df7304a37092d89e687f961cc4c71cfe7783b
dragoneye65 <john@activsoft.no>
2022-08-20 01:31:40 +0200

    Refactor and added scaling with radius

---
+++ include/Clock.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <chrono>
+#include <ctime>
+#include <string>
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include "PGE.h"
+
+class Clock {
+public:
+	Clock(olc::PixelGameEngine* engine) {
+		pge = engine;
+	}
+
+	void Draw(olc::vi2d pos);
+	void setRadius(int r) {
+		clockRadius = r;
+	}
+
+private:
+	std::time_t currentTime;
+	std::tm now;
+	std::string strTime;
+	float secRes{ 360.0f / 60.0f };
+	float minRes{ 360.0f / 60.0f };
+	float hourRes{ 360.0f / 12.0f };
+	float secRadius{ 70.0f };
+	float minRadius{ 70.0f };
+	float hourRadius{ 45.0f };
+	float clockRadius{ 90.0f };
+	olc::vi2d origo;
+	float scale{ 1.0f };
+	olc::PixelGameEngine* pge;
+	bool showDigital{ false };
+	bool showBigFourNumbers{ false };
+
+};
---
+++ src/PGE.cpp
@@ -0,0 +1,6 @@
+
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+// #define OLC_PGE_GRAPHICS2D
+// #include "olcPGEX_Graphics2D.h"
